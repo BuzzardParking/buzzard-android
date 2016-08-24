5f088c2a7539d77190786ab9b9dcdaea74ec5273
Nathan Sass <nsass711@gmail.com>
2016-08-24 12:20:14 -0700

    Moved & renamed the cluster manager to a new file.

--- app/src/main/java/com/buzzardparking/buzzard/util/MarkerManager.java
+++ app/src/main/java/com/buzzardparking/buzzard/util/MarkerManager.java
@@ -1,16 +1,8 @@
 package com.buzzardparking.buzzard.util;
 
 import android.animation.ValueAnimator;
-import android.content.Context;
 import android.graphics.Point;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.LayerDrawable;
-import android.graphics.drawable.ShapeDrawable;
-import android.graphics.drawable.shapes.OvalShape;
-import android.support.v4.content.ContextCompat;
-import android.util.SparseArray;
 import android.view.LayoutInflater;
-import android.view.ViewGroup;
 import android.view.animation.AccelerateDecelerateInterpolator;
 
 import com.buzzardparking.buzzard.R;
@@ -19,23 +11,16 @@ import com.buzzardparking.buzzard.models.Place;
 import com.flipboard.bottomsheet.BottomSheetLayout;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.Projection;
-import com.google.android.gms.maps.model.BitmapDescriptor;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.maps.android.SphericalUtil;
-import com.google.maps.android.clustering.Cluster;
-import com.google.maps.android.clustering.ClusterManager;
-import com.google.maps.android.clustering.view.DefaultClusterRenderer;
 import com.google.maps.android.ui.IconGenerator;
-import com.google.maps.android.ui.SquareTextView;
 
 import java.util.List;
 
 public class MarkerManager {
 
-    private ClusterManager<Place> clusterManager;
+    private com.google.maps.android.clustering.ClusterManager clusterManager;
     private MainActivity context;
 
     /**
@@ -110,92 +95,10 @@ public class MarkerManager {
         });
     }
 
-    private class PlaceClusterRenderer extends DefaultClusterRenderer<Place> {
-        MainActivity context;
-        private final IconGenerator iconGenerator;
-        private SparseArray<BitmapDescriptor> mIcons = new SparseArray();
-        private final float density;
-        private ShapeDrawable coloredCircleBackground;
-
-        public PlaceClusterRenderer(MainActivity context) {
-            super(context, context.getMap(), clusterManager);
-            this.context = context;
-            this.density = context.getResources().getDisplayMetrics().density;
-            this.iconGenerator =  new IconGenerator(context);
-            this.iconGenerator.setContentView(this.makeSquareTextView(context));
-            this.iconGenerator.setTextAppearance(
-                    com.google.maps.android.R.style.ClusterIcon_TextAppearance);
-            this.iconGenerator.setBackground(this.makeClusterBackground());
-        }
-
-        @Override
-        protected void onBeforeClusterItemRendered(Place place, MarkerOptions markerOptions) {
-            markerOptions
-                    .position(place.getLatLng())
-                    .alpha(getAlpha(place))
-                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.carmarker));
-        }
-
-        @Override
-        protected void onBeforeClusterRendered(Cluster<Place> cluster, MarkerOptions markerOptions) {
-
-            int clusterColor = ContextCompat.getColor(context, R.color.colorPrimary);
-
-            int bucket = this.getBucket(cluster);
-            BitmapDescriptor descriptor = this.mIcons.get(bucket);
-            if(descriptor == null) {
-                this.coloredCircleBackground.getPaint().setColor(clusterColor);
-                descriptor = BitmapDescriptorFactory.fromBitmap(
-                        this.iconGenerator.makeIcon(this.getClusterText(bucket)));
-                this.mIcons.put(bucket, descriptor);
-            }
-
-            markerOptions.icon(descriptor);
-        }
-
-        private SquareTextView makeSquareTextView(Context context) {
-            SquareTextView squareTextView = new SquareTextView(context);
-            squareTextView.setTextSize(50.0F);
-            ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(-2, -2);
-            squareTextView.setLayoutParams(layoutParams);
-            squareTextView.setId(com.google.maps.android.R.id.text);
-
-            int textPadding = (int)(20.0F * this.density);
-            squareTextView.setPadding(textPadding, textPadding, textPadding, textPadding);
-            return squareTextView;
-        }
-
-        private LayerDrawable makeClusterBackground() {
-            // Outline color
-            int clusterOutlineColor = ContextCompat.getColor(context, android.R.color.white);
-
-            this.coloredCircleBackground = new ShapeDrawable(new OvalShape());
-            ShapeDrawable outline = new ShapeDrawable(new OvalShape());
-            outline.getPaint().setColor(clusterOutlineColor);
-            LayerDrawable background = new LayerDrawable(
-                    new Drawable[]{outline, this.coloredCircleBackground});
-            int strokeWidth = (int)(this.density * 3.0F);
-            background.setLayerInset(1, strokeWidth, strokeWidth, strokeWidth, strokeWidth);
-            return background;
-        }
-    }
-
-    public float getAlpha(Place place) {
-        long age = place.getAgeInMinutes();
-//        Log.v("DEBUG", "Marker Age: " + age);
-        if (age < 5 ) {
-            return 1.0f;
-        } else if (age < 10) {
-            return 0.7f;
-        } else {
-            return 0.5f;
-        }
-    }
-
     public void setUpClusterer(GoogleMap map, MainActivity context) {
 
-        clusterManager = new ClusterManager<>(context, map);
-        clusterManager.setRenderer(new PlaceClusterRenderer(context));
+        clusterManager = new com.google.maps.android.clustering.ClusterManager(context, map);
+        clusterManager.setRenderer(new ClusterPlaceManager(context, clusterManager));
 
         map.setOnCameraChangeListener(clusterManager);
 //        onMarkerClick(map, context);
