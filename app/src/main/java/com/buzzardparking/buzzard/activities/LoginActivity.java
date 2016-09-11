e58bc735486acf3e3a0388f5037ea0a8dbf2b429
David Lee <ambitiouspear@gmail.com>
2016-09-11 10:48:16 -0700

    Add helper function to log key hash for Facebook login

--- app/src/main/java/com/buzzardparking/buzzard/activities/LoginActivity.java
+++ app/src/main/java/com/buzzardparking/buzzard/activities/LoginActivity.java
@@ -1,8 +1,13 @@
 package com.buzzardparking.buzzard.activities;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 
 import com.buzzardparking.buzzard.R;
@@ -15,6 +20,9 @@ import com.facebook.ProfileTracker;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import cn.refactor.smileyloadingview.lib.SmileyLoadingView;
 
 public class LoginActivity extends FragmentActivity {
@@ -25,8 +33,9 @@ public class LoginActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_login);
+
+//        logKeyHash();
 
         smileyLoadingView = (SmileyLoadingView) findViewById(R.id.smileyLoadingView);
         loginButton = (LoginButton) findViewById(R.id.login_button);
@@ -42,7 +51,6 @@ public class LoginActivity extends FragmentActivity {
                         protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
                             this.stopTracking();
                             Profile.setCurrentProfile(currentProfile);
-
                         }
                     };
                     profileTracker.startTracking();
@@ -56,9 +64,25 @@ public class LoginActivity extends FragmentActivity {
 
                 @Override
                 public void onError(FacebookException error) {
-
                 }
             });
+        }
+    }
+
+    public void logKeyHash() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.buzzardparking.buzzard",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
         }
     }
 
@@ -77,9 +101,6 @@ public class LoginActivity extends FragmentActivity {
         loginButton.setVisibility(View.GONE);
         smileyLoadingView.setVisibility(View.VISIBLE);
         smileyLoadingView.start();
-
-
-
         Profile profile = Profile.getCurrentProfile();
         Intent intent = new Intent(getApplicationContext(), MapActivity.class);
         intent.putExtra("userId", profile.getId());
